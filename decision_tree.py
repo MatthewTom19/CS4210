f6cd2d229bf0d60e55ac9c774cad0b0f3995c9ae
MatthewTom19 <matthewt19001@gmail.com>
2025-09-19 00:34:15 -0700

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -24,34 +24,39 @@ X = []
 Y = []
 
 #reading the data in a csv file
+
 with open('contact_lens.csv', 'r') as csvfile:
   reader = csv.reader(csvfile)
   for i, row in enumerate(reader):
       if i > 0: #skipping the header
          db.append (row)
-
+        
 #encode the original categorical training features into numbers and add to the 4D array X.
 #--> add your Python code here
 
 for row in db:
     X.append(row[:-1])
     Y.append(row[-1])
-
+  
 feature_encoder = OrdinalEncoder()
+
 X = feature_encoder.fit_transform(X)
 
 #encode the original categorical training classes into numbers and add to the vector Y.
 #--> add your Python code here
 
 class_encoder = LabelEncoder()
+
 Y = class_encoder.fit_transform(Y)
-print(class_encoder.classes_)
+
 #fitting the decision tree to the data using entropy as your impurity measure
 #--> add your Python code here
 
 clf = tree.DecisionTreeClassifier(criterion='entropy')
+
 clf = clf.fit(X, Y)
 
 #plotting the decision tree
 tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes','No'], filled=True, rounded=True)
+
 plt.show()
